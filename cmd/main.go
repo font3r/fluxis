f0f92a1550427db8370e68871cea80ccc2be6a76
fonter <serfin009@gmail.com>
2025-02-16 01:26:11 +0100

    added simple tcp client/server

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+)
+
+const buffer_limit int = 1024
+
+func main() {
+	args := os.Args
+	conn, err := net.Dial("tcp4", "0.0.0.0:5845")
+	if err != nil {
+		fmt.Println("dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(args[1]))
+	if err != nil {
+		fmt.Println("write to server failed ", err.Error())
+		os.Exit(1)
+	}
+
+	buffer := make([]byte, buffer_limit)
+	res := strings.Builder{}
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				fmt.Printf("ERROR: error reading from connection - %s\n", err)
+			}
+
+			fmt.Println("DEBUG: end of input")
+			break
+		}
+
+		res.Write(buffer[:n])
+
+		if n < buffer_limit {
+			break
+		}
+	}
+
+	fmt.Printf("> %s", res.String())
+	conn.Close()
+}
--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+)
+
+const port = 5845
+const buffer_limit int = 1024
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("INFO: starting fluxis server")
+
+	ln, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: error during binding to port %d\n", port))
+	}
+
+	defer ln.Close()
+	fmt.Printf("INFO: listening on %d\n", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("ERROR: during accepting connection from %v\n", err.Error())
+			continue
+		}
+
+		fmt.Printf("DEBUG: accepted connection from %s\n", conn.RemoteAddr())
+
+		req := strings.Builder{}
+		buffer := make([]byte, buffer_limit)
+
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					fmt.Printf("ERROR: error reading from connection - %s\n", err)
+				}
+
+				fmt.Println("DEBUG: end of input")
+				break
+			}
+
+			req.Write(buffer[:n])
+
+			if n < buffer_limit {
+				break
+			}
+		}
+
+		fmt.Printf("INFO: received %s\n", req.String())
+
+		res := strings.Builder{}
+		res.WriteString(req.String())
+
+		_, err = conn.Write([]byte("asd"))
+		if err != nil {
+			fmt.Printf("ERROR: errror during writing data to connection - %s\n", err)
+		}
+
+		fmt.Printf("DEBUG: closing connection %s\n", conn.RemoteAddr())
+		conn.Close()
+	}
 }
